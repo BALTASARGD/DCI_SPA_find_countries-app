e489c8e048d40a42247d8e4d391822ed2a034feb
BALTASARGD <https://github.com/BALTASARGD>
2025-02-20 11:17:06 +0100

    Refactor project configuration: update scripts in package.json, exclude node_modules in tsconfig.json, and simplify vite.config.ts

--- DCI_SPA_find_countries-app/vite.config.ts
+++ DCI_SPA_find_countries-app/vite.config.ts
@@ -1,21 +1,8 @@
-import { defineConfig } from 'vite'
-
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    port: 5173, // Puerto donde Vite se ejecutará
-    open: true,  // Abrir automáticamente en el navegador
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Alias para las rutas de la carpeta src
-    },
-  },
-  build: {
-    outDir: 'dist', // Directorio de salida para la build
-    sourcemap: true, // Generar mapa fuente
-  },
-  
-  
   base: '/my-app/',
-})
+  build: {
+    outDir: 'dist'
+  }
+});
